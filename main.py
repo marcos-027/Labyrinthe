6e61e9493a5159930c74196cecb711e56d1b931b
marcos-027 <marcos.dcstr@eduge.ch>
2025-05-14 23:24:21 +0200

    Update :  Nouvelle approche de résolution
    Après avoir essayé et échoué d'appliquer une méthode de résolution qui se basait sur "simplement" suivre le mur de gauche/droite, nous voulions essayé de utilisé une méthode basé sur un algorithme. En effet, l'algorithme de Pledge est bien connu pour la résolution de labyrinthes et nous avons déjà confirmer sa faisabilité dans le labyrinthe imprimé dans le cour. 
    
    Ce que ce code fait c'est de basique ment compter les virages/angles : + ou - quand on tourne à gauche ou droite. Quand le compteur atteint 0, le robot suit une ligne droite jusqu'à atteindre le mur. Nous avons essayé d'implémenter ça ici. Voici la première version de teste

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 ''' Auteur(s)   : Elias Atlab et Marcos Vinicius de Castro de facanha
     Librairie   : https://github.com/GBSL-Informatik/maqueen-plus-v2-mpy/tree/main
-    Date        : 26 mars 2025
-    Version     : 0.1
-    Description : labyrinth
+    Date        : 14 mai 2025
+    Version     : 1.0
+    Description : labyrinthe
 
    /''''^''''\
   /  L1 M R1  \
@@ -11,113 +11,77 @@
 o|.............|o
 
 '''
-from maprincess import *
 from microbit import *
-import utime
+from maqueen import *
+import time
 
-WHITE = 0
+# Constantes 
+SPEED = 30
+TURN_TIME = 2 
 BLACK = 1
 
+# Variables
+turn_counter = 0  # Compteur de virages 
 
-SPEED = 15
-TimeToTurnRight = 2000
-TimeToTurnLeft = 2000
-TURNING_SPEED= 30
-
-# line_sensor_data_all() renvoie valeur rgb 0;255 ATTENTION L2 et R2 inversé dans la librairie
-
-led_rgb(Color.WHITE)
-
-def tourner_droite90():
-    led_rgb(Color.GREEN)
-    motor_stop()
-    sleep(200)
-    motor_run(Motor.LEFT, TURNING_SPEED, 1)
-    motor_run(Motor.RIGHT, TURNING_SPEED)
-    sleep(TimeToTurnRight) # temps mesuré pour faire 90 degré
-    motor_stop()
-
-def tourner_gauche90():
-    led_rgb(Color.RED)
-    motor_stop()
-    sleep(200)
-    motor_run(Motor.LEFT, TURNING_SPEED)
-    motor_run(Motor.RIGHT, TURNING_SPEED, 1)
-    sleep(TimeToTurnLeft) # temps mesuré pour faire 90 degré
-    motor_stop()
-    
-def tourner_gauche():
-    led_rgb(Color.RED)
-    motor_stop()
-    sleep(200)
-    while line_sensor_data(LineSensor.R1) > 120 and line_sensor_data(LineSensor.L1) > 120 and line_sensor_data(LineSensor.M) > 120:
-        motor_run(Motor.LEFT, TURNING_SPEED, 1)
-        motor_run(Motor.RIGHT, TURNING_SPEED)
-    motor_run(Motor.ALL, SPEED)
-    
-def tourner_droite():
-    led_rgb(Color.GREEN)
-    motor_stop()
-    sleep(200)
-    while line_sensor_data(LineSensor.R2) < 180: # R2 = L2 mal renseigné dans la librairie
-        motor_run(Motor.LEFT, SPEED)
-        motor_run(Motor.RIGHT, 0)
-    motor_run(Motor.ALL, SPEED)
-
-def tournerD():
-    led_rgb(Color.GREEN)
-    motor_stop()
-    sleep(100)
+def avant():
     motor_run(Motor.LEFT, SPEED)
-    motor_run(Motor.RIGHT, 0)
-    sleep(500)
-    
-def tournerG():
-    led_rgb(Color.RED)
-    motor_stop()
-    sleep(100)
-    motor_run(Motor.LEFT, 0)
     motor_run(Motor.RIGHT, SPEED)
-    sleep(500)
 
+def stop():
+    motor_stop()
 
+def tourner_gauche():
+    motor_run(Motor.LEFT, SPEED, 1)
+    motor_run(Motor.RIGHT, SPEED)
+    time.sleep(TURN_TIME)
+    stop()
+    global turn_counter
+    turn_counter += 1  # +90°
 
-# while True:
-#     if line_sensor(LineSensor.R2) == BLACK :
-#         motor_run(Motor.ALL, SPEED)
-#         led_rgb(Color.WHITE)
-#         sleep(500)
-#         
-#     elif line_sensor(LineSensor.L1) == BLACK or line_sensor(LineSensor.R1) == BLACK or line_sensor(LineSensor.M) == BLACK :
-#         tournerG()
-#         
-#             
-#     elif line_sensor(LineSensor.R2) == WHITE :
-#         tournerD()
-while True:
-    if line_sensor(LineSensor.R2) == BLACK :
-        motor_run(Motor.ALL, SPEED)
+def tourner_droite():
+    motor_run(Motor.LEFT, SPEED)
+    motor_run(Motor.RIGHT, SPEED, 1)
+    time.sleep(TURN_TIME)
+    stop()
+    global turn_counter
+    turn_counter -= 1  # -90°
 
+def suivre_mur_droit():
+    global turn_counter
+    
+    if line_sensor(LineSensor.R2) == BLACK:
+        tourner_droite()
+        if line_sensor(LineSensor.M) == BLACK:
+            tourner_gauche()  
+    
+    if line_sensor(LineSensor.M) != BLACK:
+        avant()
+        time.sleep(2)
+    
+    else:
+        tourner_gauche()
+        if line_sensor(LineSensor.M) == BLACK:  
+            tourner_gauche()
+            tourner_gauche()
+
+def resoudre_labyrinthe():
+    global turn_counter
+    while True:
+        if turn_counter == 0 and line_sensor(LineSensor.M) != BLACK:
+            avant()
+            time.sleep(2)
+        else:
+            suivre_mur_droit()
         
-    elif line_sensor(LineSensor.L1) == BLACK :
-        motor_run(Motor.LEFT, 0)
-        motor_run(Motor.RIGHT, SPEED)
-    elif line_sensor(LineSensor.R1) == BLACK :
-        motor_run(Motor.LEFT, 0)
-        motor_run(Motor.RIGHT, SPEED)
-    elif line_sensor(LineSensor.M) == BLACK :
-        motor_run(Motor.LEFT, 0)
-        motor_run(Motor.RIGHT, SPEED)
-        
-            
-    elif line_sensor(LineSensor.R2) == WHITE :
-        motor_run(Motor.LEFT, SPEED)
-        motor_run(Motor.RIGHT, 0)
-    sleep(100)
-    motor_run(Motor.ALL, 0)
-        
+        if button_a.is_pressed():
+            break
 
-# while True :
-#     print (line_sensor_data_all())
 
+display.show("P")
+time.sleep(1)
+
+resoudre_labyrinthe()
+
+display.show("S")
+stop()
 
